4b4ececd55479425b853651e09451f306e24d8a7
alzablotsky <al.zablotsky@gmail.com>
2018-07-26 11:56:14 +0300

    for submission

---
+++ src/main/java/com/jb/couponsystem/Coreproject2Application.java
@@ -0,0 +1,12 @@
+package com.jb.couponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coreproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coreproject2Application.class, args);
+	}
+}
